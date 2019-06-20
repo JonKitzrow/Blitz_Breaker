608a88c6dd7799303cda174dd0d5ab4cb20a9005
Jonathan Kitzrow <jonkitzrow@gmail.com>
2019-06-20 00:27:38 -0500

    Add UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,12 +11,17 @@ public class GameController : MonoBehaviour
     float paddleRotation, screenToWorld;
     public float paddleRotationSpeed = 10;
     public float paddleRotationThreshold = 16;
+    public float ballSpeed = 3f;
+    public Canvas gameOverUI, gameRunningUI;
+    public Text scoreText, gameOverScore;
 
     // Start is called before the first frame update
     void Start()
     {
       screenToWorld = 17.8f / Screen.width; // conversion factor to go from screen to world coordinates
-      ball.GetComponent<Rigidbody2D>().velocity = new Vector3(0, -3, 0);
+      ball.GetComponent<Rigidbody2D>().velocity = new Vector3(Random.Range(-0.3f, 0.3f), Random.Range(0, 1f), 0) * ballSpeed;
+      gameRunningUI.enabled = true;
+      gameOverUI.enabled = false;
     }
 
     // Update is called once per frame
@@ -67,16 +72,26 @@ public class GameController : MonoBehaviour
       paddleParent.eulerAngles = Vector3.forward * paddleRotation;
 
       // check whether ball is out of bounds
-      if (ball.position.y < -6)
+      if (ball.position.y < -6f)
       {
+        Destroy(ball.gameObject);
         gameOver();
       }
+
+      if (ball.GetComponent<Rigidbody2D>().velocity.magnitude != ballSpeed)
+      {
+        ball.GetComponent<Rigidbody2D>().velocity = ball.GetComponent<Rigidbody2D>().velocity * ballSpeed / ball.GetComponent<Rigidbody2D>().velocity.magnitude;
+      }
+
+      scoreText.text = score.ToString();
     }
 
     public void gameOver()
     {
-
-      StartCoroutine(nextSceneOnClick());
+      gameOverScore.text = score.ToString();
+      gameRunningUI.enabled = false;
+      gameOverUI.enabled = true;
+      // StartCoroutine(nextSceneOnClick());
     }
 
     public void addScore(int addThis)
@@ -86,6 +101,10 @@ public class GameController : MonoBehaviour
 
     IEnumerator nextSceneOnClick()
     {
+      while (Input.anyKey)
+      {
+        yield return null;
+      }
       while (!Input.anyKey && !Input.GetMouseButtonDown(0))
       {
         yield return null;
